6a3f8c9e77297f9c9bd17caf4a35f73024e641be
Mariyq1 <m4697597@gmail.com>
2023-03-16 17:58:13 +0100

    Add cars from input

--- src/components/screens/home/Home.jsx
+++ src/components/screens/home/Home.jsx
@@ -1,13 +1,15 @@
-import {cars} from './cars.data.js'
+import {cars as carsData} from './cars.data.js'
 import CarItem from './car-item/CarItem';
 import CreateCarForm from './create-car-form/CreateCarForm.jsx';
+import { useState } from 'react';
 
 
 function Home() {
+  const [cars, setCars] = useState(carsData)
   return (
     <div >
       <h1>Cars Catalog</h1>
-      <CreateCarForm/>
+      <CreateCarForm setCars={setCars}/>
       {cars.length ? (
         cars.map(car=>(
             <CarItem key={car.id} car={car}/>
--- src/components/screens/home/create-car-form/CreateCarForm.jsx
+++ src/components/screens/home/create-car-form/CreateCarForm.jsx
@@ -1,9 +1,49 @@
+import { useState } from 'react'
 import styles from './CreateCarForm.module.css'
 
-const CreateCarForm = () => {
+
+const clearData = {
+    price: '',
+    name: '',
+    image: '',
+}
+const CreateCarForm = ({setCars}) => {
+    const [data, setData] = useState({
+        price: '', name: '', image: '',
+    })
+    
+    const createCar = (e)=>{
+        e.preventDefault()
+        setCars(prev => [...prev, {id:prev.length +1, ...
+        data}])
+        setData(clearData)
+    }
     return (
     <form className={styles.form}>
-        <input placeholder="Name"/>
+        <input 
+            placeholder="Name" 
+            onChange={e => setData(prev => ({
+                ...prev, name: e.target.value
+            }))}
+            value={data.name}
+            />
+        <input 
+        placeholder="Price"
+        onChange={e => setData(prev => ({
+                ...prev, price: e.target.value
+            }))}
+        value={data.price}
+        />
+        <input 
+        placeholder="Image"
+        onChange={e => setData(prev => ({
+                ...prev, image: e.target.value
+            }))}
+        value={data.image}
+        />
+        <button 
+        onClick={e=>createCar(e)}
+        className="btn">Create</button>
     </form>
     )
 }
